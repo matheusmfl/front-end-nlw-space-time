056178b29fc465b2d4eeb3df3ea79fd81718ec05
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-19 23:51:03 -0300

    Create first page, create stripes background, create blur background, create personalized styles tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,29 @@
-
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className="grid grid-cols-2 min-h-screen">
+      {/* Left */}
+      <div className=" relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
+        {/* blur */}
+        <div
+          className="absolute right-0 top-1/2 h-[288px] blur-full
+        w-[526px] -translate-y-1/2 rounded-full -translate-x-1/2 bg-purple-700 opacity-50"
+        ></div>
+        {/* Stripes */}
+        <div className="absolute right-0 top-0 bottom-0 w-2 bg-stripes" />
+      </div>
+      {/* Right */}
+      <div className="flex flex-col p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="text-center leading-relaxed w-[360px]">
+            Você ainda não registrou nenhuma lembrança. comece a{' '}
+            <a href="" className="underline hover:text-gray-50">
+              {' '}
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
