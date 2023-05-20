903bea6539d8c908b1e3a8ba0029472e44539211
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-19 23:58:48 -0300

    Fix bugs on styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className=" relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden">
+      <div className=" relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10">
         {/* blur */}
         <div
           className="absolute right-0 top-1/2 h-[288px] blur-full
-        w-[526px] -translate-y-1/2 rounded-full -translate-x-1/2 bg-purple-700 opacity-50"
+        w-[526px] -translate-y-1/2 rounded-full translate-x-1/2 bg-purple-700 opacity-50"
         ></div>
         {/* Stripes */}
-        <div className="absolute right-0 top-0 bottom-0 w-2 bg-stripes" />
+        <div className="absolute right-2  top-0 bottom-0 w-2 bg-stripes" />
       </div>
       {/* Right */}
       <div className="flex flex-col p-16">
