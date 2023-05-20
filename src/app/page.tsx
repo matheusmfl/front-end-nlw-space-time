ad90a843de66f9c87d4f87796470b6416153c864
Matheus Fonteles <92957086+matheusmfl@users.noreply.github.com>
2023-05-20 18:35:24 -0300

    Add bg-stars, add logo, create antialiazed fonts, create leftside homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+import { User } from 'lucide-react'
+import Image from 'next/image'
+import logo from '../assets/logo.svg'
+
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 min-h-screen">
+    <main className="bg-[url(../assets/bg-stars.svg)] bg-cover grid grid-cols-2 min-h-screen">
       {/* Left */}
       <div className=" relative flex flex-col items-start justify-between px-28 py-16 overflow-hidden border-r border-white/10">
         {/* blur */}
@@ -10,9 +14,42 @@ export default function Home() {
         ></div>
         {/* Stripes */}
         <div className="absolute right-2  top-0 bottom-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href=""
+          className="flex hover:text-gray-50 transition-colors items-center gap-3 text-left"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={logo} alt="" />
+
+          <div className="max-w-[420px] space-y-1">
+            <h1 className="mt-5 text-4xl font-bold leading-tight text-gray-50">
+              Sua capsula do tempo
+            </h1>
+            <p>
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+
+            <a href="" className="block uppercase pt-5">
+              Cadastrar lembranças
+            </a>
+          </div>
+        </div>
       </div>
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
             Você ainda não registrou nenhuma lembrança. comece a{' '}
